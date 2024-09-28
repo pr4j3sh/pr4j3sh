e93e157cb763671e63c7606dd6d51d22f8c73f11
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-09-28 20:14:21 +0530

    rm build

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/05-vite-github-pages_CcsCBI-v.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./05-vite-github-pages_sYWoNX7P.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/deepthought_D6Y3BCoO.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./deepthought_PB1UiF0N.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
