134d86d9cb83bf985c0ed7fd8d307a67d80e9131
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-09-28 18:36:11 +0530

    added fbase

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbrsVSz6Q7_cnU6INu-TKJrxHuwCacfyg",
+  authDomain: "mine-7f634.firebaseapp.com",
+  projectId: "mine-7f634",
+  storageBucket: "mine-7f634.appspot.com",
+  messagingSenderId: "157489452148",
+  appId: "1:157489452148:web:4818c04da1d0ed5831ed21",
+  measurementId: "G-17P6B4YJJQ",
+};
+
+export const app = initializeApp(firebaseConfig);
