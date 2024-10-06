51ef045d3972a389b295b866a2b7ffe6e1fe56f6
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-10-06 19:57:54 +0530

    added analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbrsVSz6Q7_cnU6INu-TKJrxHuwCacfyg",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
