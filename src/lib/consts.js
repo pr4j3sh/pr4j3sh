a848784d6d8591e8f6dfc9abe7aaa69ed152ab6b
pr4j3sh <prajesh.eleven118@gmail.com>
2025-02-24 00:54:49 +0530

    rm

---
+++ src/lib/consts.js
@@ -0,0 +1,16 @@
+export const SITE = {
+  TITLE: "Prajesh Pratap Singh",
+  DESCRIPTION:
+    "A Vanilla Javascript + Tailwind CSS template with pre-configured styles, dark theme, svg icon support and fully responsive.",
+  URL: "https://pr4j3sh.github.io/pr4j3sh/",
+  REPO: "pr4j3sh",
+};
+
+export const AUTHOR = {
+  NAME: "Prajesh Pratap Singh",
+  USERNAME: "pr4j3sh",
+  EMAIL: "prajesh.eleven118@gmail.com",
+  URL: "https://pr4j3sh.vercel.app/",
+  TWITTER: "https://x.com/pr4j3sh",
+  GITHUB: "https://github.com/pr4j3sh",
+};
